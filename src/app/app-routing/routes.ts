98172946ad1dbfcd796e7ce81d3f1ee72573a9cb
oussema hasnaoui <oussema.hasnaoui@esprit.tn>
2020-08-04 03:46:37 +0100

    Single Page Applications Part 1

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -1,14 +1,14 @@
-import {Routes} from '@angular/router';
+import { Routes } from '@angular/router';
+
+import { MenuComponent } from '../menu/menu.component';
+import { DishdetailsComponent } from '../dishdetails/dishdetails.component';
 import { HomeComponent } from '../home/home.component';
 import { AboutComponent } from '../about/about.component';
-import { DishdetailsComponent } from '../dishdetails/dishdetails.component';
-import { MenuComponent } from '../menu/menu.component';
 import { ContactComponent } from '../contact/contact.component';
 
-export  const  routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'menu', component: MenuComponent},
-  {path: '', redirectTo: '/home', pathMatch: 'full'}
-
-
+export const routes: Routes = [
+  { path: 'home',  component: HomeComponent },
+  { path: 'menu',     component: MenuComponent },
+  { path: 'contactus',     component: ContactComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import {AppRoutingModule} from './app-routing/app-routing.module';
+import {PromotionService} from './services/promotion.service';
 
 @NgModule({
   declarations: [
@@ -43,7 +44,8 @@ import {AppRoutingModule} from './app-routing/app-routing.module';
 
   ],
   providers: [
-    DishService
+    DishService,
+    PromotionService
   ],
   bootstrap: [AppComponent]
 })
