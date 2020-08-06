4ae6f3f7f79808a2b8a1731284cd50a43c0e4998
oussema hasnaoui <oussema.hasnaoui@esprit.tn>
2020-08-06 16:02:38 +0100

    final week 2

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -11,6 +11,7 @@ export const routes: Routes = [
   { path: 'menu',     component: MenuComponent },
   { path: 'contactus',     component: ContactComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'dishdetail/:id',     component: DishdetailsComponent }
+  { path: 'dishdetail/:id',     component: DishdetailsComponent },
+  {path: 'about', component: AboutComponent}
 
 ];
